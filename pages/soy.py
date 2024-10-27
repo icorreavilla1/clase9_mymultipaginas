fbe85470e1ade54eabd6ee1e1e7a8f4a5c52bc17
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-10-27 16:52:34 -0500

    Create soy.py

---
+++ pages/soy.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    st.title("Bienvenido a la Aplicación de Reconocimiento")
+    st.write("Usa el menú en la barra lateral para navegar entre las diferentes páginas.")
+   # st.image("path/to/your/home_image.png")  # Puedes agregar una imagen si lo deseas
