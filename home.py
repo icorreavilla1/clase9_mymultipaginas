17e6bab447fdfc11140722af7b12a33703a603f2
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-10-27 16:55:25 -0500

    Update home.py

--- home.py
+++ home.py
@@ -4,10 +4,3 @@ from pages import soy
 st.title("HOME🏡")
 
 st.sidebar.title("Navegación")
-page = st.sidebar.radio("Ir a:", ["Quien soy"])
-
-# Lógica para mostrar cada página
-if page == "Inicio":
-    home.show()
-elif page == "Quien soy":
-    about.show()
