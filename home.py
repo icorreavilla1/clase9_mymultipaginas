be6927c871c782f626c41fcb60efb6350d7d961e
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-10-27 16:57:31 -0500

    Update home.py

--- home.py
+++ home.py
@@ -2,5 +2,8 @@ import streamlit as st
 from pages import soy 
 
 st.title("HOME🏡")
+st.write("""
+    BIENVENIDO A MI APP MULTIPÁGINAS
+    """)
 
-st.sidebar.title("Navegación")
+st.sidebar.title("En estas páginas podrás conocer un poco más sobre mí 🤩🍀✨🌼")
