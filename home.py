1b6d596dd70b356fde3702012bebd51fb1e14efb
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-10-27 16:54:57 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from pages import soy 
 
 st.title("HOME🏡")
+
+st.sidebar.title("Navegación")
+page = st.sidebar.radio("Ir a:", ["Quien soy"])
+
+# Lógica para mostrar cada página
+if page == "Inicio":
+    home.show()
+elif page == "Quien soy":
+    about.show()
